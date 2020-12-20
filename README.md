e21ebee74e34169bea42b04031fbaa73a378cf95
Matheus A.F <matheus.alvfr@gmail.com>
2020-12-20 15:35:24 -0300

    Updating logic

--- README.md
+++ README.md
@@ -1,21 +1,89 @@
-# Mega
+# Descrição
 
-**TODO: Add description**
+A aplicação agrega os números que mais foram sorteados na Mega-Sena desde 1996.
 
-## Installation
+A fonte dos dados é o [site da loterial](http://loterias.caixa.gov.br)
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `mega` to your list of dependencies in `mix.exs`:
 
-```elixir
-def deps do
-  [
-    {:mega, "~> 0.1.0"}
-  ]
-end
+# Utilização
+
+Para executar o script, acesse o terminal e abra uma sessão interativa do Mix com 
+
+```
+  iex -S mix
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/mega](https://hexdocs.pm/mega).
+Execute a função principal
+
+```
+  Mega.main
+```
+
+# Resultados 
+
+Os resultados até hoje (20/12/2020) são os seguintes:
+
+
+| Número  |  Quantidade  |
+| ---------- | ---- |
+| 10 | 266 |
+| 53 | 265 |
+| 5 |257   | 
+| 33 | 256 |
+| 23 | 254 |
+| 42 | 252 |
+| 4 | 251 |
+| 37 | 250 |
+| 27 | 250 |
+| 54 | 248 |
+| 28 | 248 |
+| 30 | 247 |
+| 34 | 245 |
+| 43 | 245 |
+| 24 | 245 |
+| 17 | 242 |
+| 35 | 241 |
+| 16 | 241 |
+| 44 | 240 |
+| 36 | 240 |
+| 6 | 240 |
+| 41 | 240 |
+| 29 | 240 |
+| 51 | 239 |
+| 52 | 238 |
+| 2 | 238 |
+| 56 | 238 |
+| 11 | 237 |
+| 13 | 236 |
+| 32 | 235 |
+| 38 | 235 |
+| 49 | 234 |
+| 18 | 234 |
+| 50 | 233 |
+| 8 | 232 |
+| 1 | 231 |
+| 58 | 231 |
+| 46 | 231 |
+| 45 | 229 |
+| 12 | 229 |
+| 59 | 227 |
+| 20 | 224 |
+| 47 | 224 |
+| 40 | 224 |
+| 14 | 222 |
+| 7 | 221 |
+| 57 | 221 |
+| 39 | 220 |
+| 31 | 219 |
+| 19 | 218 |
+| 60 | 217 |
+| 25 | 215 |
+| 9 | 214 |
+| 48 | 212 |
+| 3 | 212 |
+| 15 | 206 |
+| 21 | 203 |
+| 22 | 202 |
+| 55 | 197 |
+| 26 | 193 |
 
--- lib/mega.ex
+++ lib/mega.ex
@@ -1,48 +1,50 @@
 defmodule Mega do
   use Task, restart: :permanent
 
+  def main() do
+    tasks =
+      1..get_last_game()
+      |> Task.async_stream(fn req -> req |> run end, timeout: 500_000)
+
+    tasks |> Enum.map(fn task -> elem(task, 1) end) |> process_data
+  end
+
+  defp process_data(data) do
+    data
+    |> List.flatten()
+    |> Enum.filter(fn elm -> elm != nil end)
+    |> Enum.map(fn i -> elem(Integer.parse(i), 0) end)
+    |> most_common
+    |> format
+  end
+
+  defp most_common(list) when is_list(list) do
+    list |> Enum.reduce(%{}, fn val, acc -> Map.update(acc, val, 1, &(&1 + 1)) end)
+  end
+
+  defp to_keyword(map) do
+    map |> Enum.map(fn {k, v} -> {String.to_atom(Integer.to_string(k)), v} end)
+  end
+
+  defp format(map) do
+    map
+    |> to_keyword
+    |> List.keysort(1)
+    |> Enum.reverse()
+    |> print
+  end
+
+  defp print(kw_list) do
+    kw_list |> Enum.each(fn {k, v} -> IO.puts("#{k} => #{v}") end)
+  end
+
   defp get_last_game() do
     data = Client.get_last_result() |> Parser.parse()
     data["numero"]
   end
 
-  def run(number) do
+  defp run(number) do
     data = number |> Client.get_result() |> Parser.parse()
     data["listaDezenas"]
-  end
-
-  def main() do
-    last_game = get_last_game()
-
-    tasks =
-      1..5
-      |> Task.async_stream(fn req -> req |> run end, timeout: 100_000)
-
-    tasks |> Enum.map(fn x -> elem(x, 1) end) |> process_data
-    # Task.await_many(tasks, 100_000) |> process_data()
-  end
-
-  def process_data(data) do
-    data |> List.flatten() |> Enum.map(fn i -> elem(Integer.parse(i), 0) end)
-  end
-
-  def most_common(list) when is_list(list) do
-    {amts, highest_count, highest_values} =
-      Enum.reduce(list, {%{}, 0, []}, fn v, {a, hc, hv} ->
-        case Map.update(a, v, 1, &(&1 + 1)) do
-          %{^v => ^hc} = a -> {a, hc, [v | hv]}
-          %{^v => c} = a when c > hc -> {a, c, [v]}
-          a -> {a, hc, hv}
-        end
-      end)
-
-    # Now we have all the interesting bits, the example code in the first post just prints though...
-    if map_size(amts) == length(highest_values) do
-      IO.puts("All elements have the same frequency in list: #{inspect(list)}")
-    else
-      IO.puts(
-        "Most common elements are #{inspect(highest_values)} with frequency #{highest_count}"
-      )
-    end
   end
 end
