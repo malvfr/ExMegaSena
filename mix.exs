3bd7697cacdd136c0c71b756f36e9ba6b85841d5
Matheus A.F <matheus.alvfr@gmail.com>
2020-12-24 15:17:05 -0300

    Publishing to hex

--- mix.exs
+++ mix.exs
@@ -6,8 +6,10 @@ defmodule Mega.MixProject do
       app: :mega,
       version: "0.1.0",
       elixir: "~> 1.11",
+      description: "Uma simples aplicação para listar ordenadamente os números mais sorteados da Mega-Sena.",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -18,13 +20,20 @@ defmodule Mega.MixProject do
     ]
   end
 
+  defp package do
+    [
+      maintainers: ["Matheus Alvarenga"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/malvfr/ExMegaSena"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:httpoison, "~> 1.7"},
-      {:poison, "~> 3.1"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:poison, "~> 3.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
